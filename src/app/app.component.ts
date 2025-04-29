ad7674637bb03b11149548b5cee6a7eb9bb69cc6
Rodrigo Tranolao <rtranolao.externo@softys.com>
2025-04-28 23:45:30 -0400

    se avanza en conexion con SQLite pero sin resultados optimos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   imports: [IonApp, IonRouterOutlet],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   constructor() {}
--- src/app/services/quotes.service.ts
+++ src/app/services/quotes.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import { CapacitorSQLite, SQLiteConnection, SQLiteDBConnection } from '@capacitor-community/sqlite';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuotesService {
+
+
   private quotes = [
     { text: 'La vida es aquello que te va sucediendo mientras estás ocupado haciendo otros planes.', author: 'John Lennon' },
     { text: 'El éxito es la suma de pequeños esfuerzos repetidos día tras día.', author: 'Robert Collier' }
@@ -11,7 +15,35 @@ export class QuotesService {
 
   private allowDeleteOnHome = false; 
 
+
+  sqlite: SQLiteConnection = new SQLiteConnection(CapacitorSQLite)
+  db!: SQLiteConnection;
+  plataforma: string      = ""
+
+  DB_NAME: string         = "list_quotes";
+  DB_ENCRIPTADA: boolean  = false;
+  DB_MODE: string         = "no-encryption";
+  DB_VERSION: number      = 1;
+  DB_READ_ONLY: boolean   = false;
+  DB_SQL_TABLAS: string   = `
+          CREATE TABLE IF NOT EXISTS quotes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL,
+            author TEXT NOT NULL
+          );
+        `;
+  DB_TABLE_NAME: any;
+
   constructor() { }
+
+  private async _iniciarPluginWeb(): Promise<void> {    
+    await customElements.whenDefined('jeep-sqlite')
+    const jeepSqliteEl = document.querySelector("jeep-sqlite")
+    if( jeepSqliteEl != null ) {      
+      await this.sqlite.initWebStore()            
+    }
+  }
+
 
   getQuotes() {
     return [...this.quotes];
